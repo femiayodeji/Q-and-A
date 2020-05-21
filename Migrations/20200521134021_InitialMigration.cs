cba4c98f97d553b80298ad32a15e2a02e1a24f1d
Femi Ayodeji <feminstincts@gmail.com>
2020-05-21 15:05:24 +0100

    added database migration

---
+++ Migrations/20200521134021_InitialMigration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Qurious.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Enquiries",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Question = table.Column<string>(maxLength: 250, nullable: false),
+                    Answer = table.Column<string>(nullable: false),
+                    Difficulty = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Enquiries", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Enquiries");
+        }
+    }
+}
