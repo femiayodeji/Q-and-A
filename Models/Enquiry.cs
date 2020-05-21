711a2713a817b9480c06695355c4416f2aba1703
Femi Ayodeji <feminstincts@gmail.com>
2020-05-21 14:39:13 +0100

    added data annotations to enquiry model

--- Models/Enquiry.cs
+++ Models/Enquiry.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Qurious.Models{
     public class Enquiry{
+        [Key]
         public int Id {get; set;}
+        [Required]
+        [MaxLength(250)]
         public string Question {get; set;}
+        [Required]
         public string Answer {get; set;}
-        public int Difficulty {get; set;}
+        public int? Difficulty {get; set;}
     }
 }
