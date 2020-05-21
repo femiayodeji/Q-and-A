3a9b62506603ce93145c58f099c0675ae655a50d
Femi Ayodeji <feminstincts@gmail.com>
2020-05-21 09:40:00 +0100

    created models and added Enquiry

---
+++ Models/Enquiry.cs
@@ -0,0 +1,8 @@
+namespace qurious.Models{
+    public class Enquiry{
+        public int Id {get; set;}
+        public string Question {get; set;}
+        public string Answer {get; set;}
+        public int Difficulty {get; set;}
+    }
+}
