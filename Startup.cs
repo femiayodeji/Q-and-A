6306dc423f666d99ac85ab8e664dce0881526370
Femi Ayodeji <feminstincts@gmail.com>
2020-05-21 14:21:33 +0100

    added connectionstring configuration

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,10 @@ namespace Qurious
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<QuriousContext>(opt =>
+            {
+                opt.UseSqlServer(Configuration.GetConnectionString("QuriousConnection"));
+            });
             services.AddControllers();
             services.AddScoped<IEnquiryRepository, MockEnquiryRepository>();
         }
